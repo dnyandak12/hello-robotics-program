1f3e799e8b5e9d11d35bc28958c0b1d1ef2aee91
dnyandak12 <dnyandakakare@gmail.com>
2025-09-29 12:25:39 +0530

    Create Program64.c

---
+++ Program64.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main() 
+{
+  int a,b,c; printf("Enter three numbers: "); scanf("%d %d %d",&a,&b,&c);
+    if(a<=b && a<=c) printf("Smallest=%d\n",a);
+    else if(b<=a && b<=c) printf("Smallest=%d\n",b);
+    else printf("Smallest=%d\n",c);
+}
