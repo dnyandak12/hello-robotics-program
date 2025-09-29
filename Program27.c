c143ebf4a44a02e568187b870c259424fdd65fbd
dnyandak12 <dnyandakakare@gmail.com>
2025-09-29 11:37:09 +0530

    Create Program27.c

---
+++ Program27.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main() 
+{
+  int n, count = 0;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    while (n != 0) {
+        count++;
+        n /= 10;
+    }
+    printf("Digits = %d\n", count);
+}
