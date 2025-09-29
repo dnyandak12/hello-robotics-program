4809baa05c4c2f4dcc4b3b57e5a520b92059b9f1
dnyandak12 <dnyandakakare@gmail.com>
2025-09-29 12:22:39 +0530

    Create Program60.c

---
+++ Program60.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main() 
+{
+  int y; printf("Enter year: "); scanf("%d",&y);
+    if((y%400==0)||(y%4==0 && y%100!=0)) printf("Leap Year\n");
+    else printf("Not Leap Year\n");
+}
