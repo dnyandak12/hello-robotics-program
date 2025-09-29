9f893d49a9c3619c870d562d8f5a4447c2283399
dnyandak12 <dnyandakakare@gmail.com>
2025-09-29 11:24:33 +0530

    Create Program9.c

---
+++ Program9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main() {
+  
+int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swap: a=%d, b=%d\n", a, b);
+}
