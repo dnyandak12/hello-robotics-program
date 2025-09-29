dc2928ccf83ea92a612a6e417ac5c6268921a28b
dnyandak12 <dnyandakakare@gmail.com>
2025-09-29 11:22:48 +0530

    Create Program8.c

---
+++ Program8.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a, b, temp;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    temp = a; a = b; b = temp;
+    printf("After swap: a=%d, b=%d\n", a, b);
+}
