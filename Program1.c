86c8dc201580bf4212e840a1624d3ea80f5d4037
dnyandak12 <dnyandakakare@gmail.com>
2025-09-29 07:46:34 +0530

    Add files via upload

---
+++ Program1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,sum;
+    a=12;
+    b=10;
+    sum=a+b;
+    clrscr();
+    printf("a+b=%d : ",sum);
+    scanf("%d %d",&sum);
+    return(0)
+}
+//for while, do while 
