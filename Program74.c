75123e53b6788c606afc3d30899fc2d3dd1287db
dnyandak12 <dnyandakakare@gmail.com>
2025-09-29 12:32:07 +0530

    Create Program74.c

---
+++ Program74.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main() 
+{
+  int a,b,c,d; printf("Enter four numbers: "); scanf("%d %d %d %d",&a,&b,&c,&d);
+    int max=a; if(b>max) max=b; if(c>max) max=c; if(d>max) max=d;
+    printf("Largest=%d\n", max);
+}
