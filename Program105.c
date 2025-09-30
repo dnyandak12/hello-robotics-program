9ee6197b039e8404e1313438e4f632f7db824ba0
dnyandak12 <dnyandakakare@gmail.com>
2025-09-30 08:08:37 +0530

    Create Program105.c

---
+++ Program105.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main() 
+{
+  int n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    if(n%3==0) printf("Divisible by 3\n");
+    else printf("Not divisible by 3\n");
+}
