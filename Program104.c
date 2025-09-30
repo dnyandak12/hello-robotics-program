2fcbc8ef520b827b35e9ef2be5fa98762ded5846
dnyandak12 <dnyandakakare@gmail.com>
2025-09-30 08:08:04 +0530

    Create Program104.c

---
+++ Program104.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main() 
+{
+  int n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    if(n%5==0) printf("Divisible by 5\n");
+    else printf("Not divisible by 5\n");
+}
