30d28ebd7c50397c3b87010a62bf14bd81702203
dnyandak12 <dnyandakakare@gmail.com>
2025-09-30 08:04:31 +0530

    Create Program97.c

---
+++ Program97.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main() 
+{
+  int n,flag=1;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    if(n<=1) flag=0;
+    for(int i=2;i<=n/2;i++){
+        if(n%i==0){flag=0; break;}
+    }
+    if(flag) printf("Prime\n");
+    else printf("Not Prime\n");
+}
