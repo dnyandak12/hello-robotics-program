f6e9073676dd594895423f0505aabdd01687828d
dnyandak12 <dnyandakakare@gmail.com>
2025-09-30 11:38:03 +0530

    Create Program154.c

---
+++ Program154.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main() 
+{
+  int n, coef=1, space, i, j;
+    printf("Enter rows: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++) {
+        for(space=1;space<=n-i;space++) printf("  ");
+        for(j=0;j<=i;j++) {
+            if(j==0||i==0) coef=1;
+            else coef=coef*(i-j+1)/j;
+            printf("%4d",coef);
+        }
+        printf("\n");
+    }
+}
