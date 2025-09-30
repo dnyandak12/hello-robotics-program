f48a2bd45e5457a39bdc3d836786fb48c8217bd1
dnyandak12 <dnyandakakare@gmail.com>
2025-09-30 08:14:52 +0530

    Create Program113.c

---
+++ Program113.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main() 
+{
+  int n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    if(n%2==0 && n%3==0) printf("Divisible by 2 and 3\n");
+    else printf("Not divisible by 2 and 3\n");
+}
