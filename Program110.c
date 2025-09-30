53b98be54b0ac87e4fc49a05c61f8ea040c030af
dnyandak12 <dnyandakakare@gmail.com>
2025-09-30 08:13:14 +0530

    Create Program110.c

---
+++ Program110.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main() 
+{
+  int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    if(a<b) printf("Smallest = %d\n",a);
+    else printf("Smallest = %d\n",b);
+}
