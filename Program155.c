b63af1b3b55bea35e4ab94dbd1064d3381a75ca7
dnyandak12 <dnyandakakare@gmail.com>
2025-09-30 11:38:46 +0530

    Create Program155.c

---
+++ Program155.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main() 
+{
+  int n, i, j, space;
+    printf("Enter rows: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++) {
+        for(space=1;space<=n-i;space++) printf(" ");
+        for(j=1;j<=2*i-1;j++) printf("*");
+        printf("\n");
+    }
+}
