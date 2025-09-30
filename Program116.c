6b69e3f39739271d1e8463fd065917e2c02e9b8b
dnyandak12 <dnyandakakare@gmail.com>
2025-09-30 08:16:46 +0530

    Create Program116.c

---
+++ Program116.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main() 
+{
+  int a,b,c;
+    printf("Enter three sides: ");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a==b && b==c) printf("Equilateral\n");
+    else if(a==b || b==c || a==c) printf("Isosceles\n");
+    else printf("Scalene\n");
+}
