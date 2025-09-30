901ffcbab5ee2b863299ac3a9a482f3cc8a6931e
dnyandak12 <dnyandakakare@gmail.com>
2025-09-30 11:41:03 +0530

    Create Program159.c

---
+++ Program159.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main() 
+{
+  int n,i,min;
+    printf("Enter size: ");
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    min=a[0];
+    for(i=1;i<n;i++) if(a[i]<min) min=a[i];
+    printf("Smallest = %d\n",min);
+}
