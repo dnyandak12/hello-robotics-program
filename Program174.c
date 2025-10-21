8d5176b1238f2787bf1f3def8ada9260002f6671
Dnyanda <dnyandakakare@gmail.com>
2025-10-21 22:50:45 +0530

    Add function to find maximum in an array

---
+++ Program174.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int maxInArray(int a[], int n){
+    int m = a[0];
+    for(int i=1;i<n;i++) if(a[i]>m) m=a[i];
+    return m;
+}
+int main(){
+    int n; scanf("%d",&n);
+    int a[n]; for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    printf("Max = %d\n", maxInArray(a,n));
+    return 0;
+}
