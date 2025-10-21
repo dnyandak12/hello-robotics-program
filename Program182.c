4bbf7548317db3b065c3e39f29648255587b1d1d
Dnyanda <dnyandakakare@gmail.com>
2025-10-21 22:54:27 +0530

    Implement selection sort in Program182.c

---
+++ Program182.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void selectionSort(int a[], int n){
+    for(int i=0;i<n-1;i++){
+        int min=i;
+        for(int j=i+1;j<n;j++) if(a[j]<a[min]) min=j;
+        int t=a[i]; a[i]=a[min]; a[min]=t;
+    }
+}
+int main(){
+    int n; scanf("%d",&n);
+    int a[n]; for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    selectionSort(a,n);
+    for(int i=0;i<n;i++) printf("%d ",a[i]);
+    printf("\n");
+    return 0;
+}
